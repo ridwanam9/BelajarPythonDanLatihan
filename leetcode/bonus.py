674b5e9a649bd1a1ba6132e928e0c65d29df01a2
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-06-12 14:17:08 +0700

    update bonus

--- leetcode/bonus.py
+++ leetcode/bonus.py
@@ -1,14 +1,26 @@
 # 10. Two Sum (LeetCode-style)
 # Diberikan list nums dan target target, kembalikan indeks dua angka yang jika dijumlahkan hasilnya target.
 
-def two_sum(nums, target):
+# # code awal
+# def two_sum(nums, target):
     
-    for x in nums:
-        for y in nums:
-            if x + y == target:
-                hasil = print(f"[{nums.index(x)}, {nums.index(y)}]")
-                return hasil
+#     for x in nums:
+#         for y in nums:
+#             if x + y == target:
+#                 hasil = print(f"[{nums.index(x)}, {nums.index(y)}]")
+#                 return hasil
 
-list_angka = [1,2,3,8,9,20,46]
-two_sum(list_angka, 5)
-two_sum(list_angka, 17)
+# list_angka = [1,2,3,8,9,20,46]
+# two_sum(list_angka, 5)
+# two_sum(list_angka, 17)
+
+# code baru
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return None
+
+print(two_sum([1,2,3,8,9,20,46], 5))   # [0, 2]
+print(two_sum([1,2,3,8,9,20,46], 17))  # [3, 4]
