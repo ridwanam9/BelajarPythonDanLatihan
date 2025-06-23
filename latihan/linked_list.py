aad5dc44b889ee622dd6eccbab014c09a05951f2
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-06-23 21:07:01 +0700

    insert at position

--- latihan/linked_list.py
+++ latihan/linked_list.py
@@ -22,6 +22,23 @@ class Linked_list:
         while last.next:
             last = last.next
         last.next = new_node
+    
+    def insert_at_position(self, index, data):
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+        new_node = Node(data)
+        current_node = self.head
+        position = 0
+        while current_node and position < index - 1:
+            current_node = current_node.next
+            position += 1
+        if not current_node:
+            print("Index not present")
+            return
+        new_node.next = current_node.next
+        current_node.next = new_node
+
 
     def print_list(self):
         current_node = self.head
@@ -37,4 +54,6 @@ lists.insert_at_end(10)
 lists.insert_at_end(20)
 lists.insert_at_end(30)
 lists.insert_at_beginning(0)
+lists.insert_at_end(40)
+lists.insert_at_position(2, 60)
 lists.print_list()
