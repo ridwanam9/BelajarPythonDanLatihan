5825249d97adc0aa513825a5c710f8f142c117a0
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-06-23 18:01:34 +0700

    linked list append and print

--- latihan/linked_list.py
+++ latihan/linked_list.py
@@ -6,3 +6,21 @@ class Node:
 class Linked_list:
     def __init__(self, head):
         self.head = head
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        # last / curret node
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("None")
