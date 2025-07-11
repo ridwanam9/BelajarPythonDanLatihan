9e9c6cf0f489f74050f16a7d7fcb87adb13747a9
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-07-12 00:37:13 +0700

    Number_of_Good_Pairs

---
+++ leetcode/Number_of_Good_Pairs.py
@@ -0,0 +1,19 @@
+# Given an array of integers nums, return the number of good pairs.
+
+# A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] == nums[j] and i < j:
+                    count += 1
+
+        return count
+    
+print(Solution().numIdenticalPairs([1,2,3,1,1,3]))
