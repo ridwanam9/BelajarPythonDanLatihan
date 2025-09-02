97717a6c2e10e54a492bc5c7464255bc38344962
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-09-02 20:56:59 +0700

    invert_binary_tree

---
+++ leetcode/invert_binary_tree.py
@@ -0,0 +1,68 @@
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return None
+        
+        # Swap left and right children
+        root.left, root.right = root.right, root.left
+        
+        # Recursively invert left and right subtrees
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
+    
+def build_tree(values):
+    if not values:
+        return None
+    root = TreeNode(values[0])
+    queue = [root]
+    i = 1
+    while queue and i < len(values):
+        node = queue.pop(0)   # ambil elemen pertama
+        if i < len(values) and values[i] is not None:
+            node.left = TreeNode(values[i])
+            queue.append(node.left)
+        i += 1
+        if i < len(values) and values[i] is not None:
+            node.right = TreeNode(values[i])
+            queue.append(node.right)
+        i += 1
+    return root
+
+# Helper: serialize tree to list (level order)
+def tree_to_list(root):
+    if not root:
+        return []
+    result = []
+    queue = [root]
+    while queue:
+        node = queue.pop(0)
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    # Hapus None di akhir
+    while result and result[-1] is None:
+        result.pop()
+    return result
+
+
+# ---- TEST ----
+root = build_tree([4,2,7,1,3,6,9])
+solution = Solution()
+new_root = solution.invertTree(root)
+print(tree_to_list(new_root))  # Expected: [4,7,2,9,6,3,1]
