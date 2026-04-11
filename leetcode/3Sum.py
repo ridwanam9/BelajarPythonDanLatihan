6b63e00f7f0f220274207a23a111e5b9b6f14a1b
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2026-04-11 22:50:21 +0700

    3Sum

---
+++ leetcode/3Sum.py
@@ -0,0 +1,89 @@
+# Given an integer array nums, return all the triplets 
+# [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+# Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+# Example 1:
+
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+# Explanation: 
+# nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+# nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+# nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+# The distinct triplets are [-1,0,1] and [-1,-1,2].
+# Notice that the order of the output and the order of the triplets does not matter.
+
+# Example 2:
+
+# Input: nums = [0,1,1]
+# Output: []
+# Explanation: The only possible triplet does not sum up to 0.
+
+# Example 3:
+
+# Input: nums = [0,0,0]
+# Output: [[0,0,0]]
+# Explanation: The only possible triplet sums up to 0.
+
+
+# import itertools
+# nums = [-1,0,1,2,-1,-4]
+# # nums = [0,1,1]
+
+# perm_3 = set(list(itertools.permutations(nums, 3)))
+
+# eq_0 = []
+# for x in perm_3:
+#     if sorted(x) not in eq_0:
+#         if sum(x) == 0:
+#             eq_0.append(sorted(list(x)))
+
+# print(eq_0)
+
+
+def threeSum(nums):
+    nums.sort()
+    result = []
+
+    for i in range(len(nums)):
+        # skip duplicate
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        left = i + 1
+        right = len(nums) - 1
+
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+
+            if total == 0:
+                result.append([nums[i], nums[left], nums[right]])
+
+                # skip duplicate
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+
+            elif total < 0:
+                left += 1
+            else:
+                right -= 1
+
+    return result
+
+
+nums = [-1,0,1,2,-1,-4]
+print(threeSum(nums))
+
+
+
+
+
+
