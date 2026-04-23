72b32551507184abc8cef8bb7772fc010e442bf0
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2026-04-24 00:02:36 +0700

    data structures

---
+++ codeforces/2210a.py
@@ -0,0 +1,7 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    
+    arr = []
+    for x in range(1, n+1):
+        arr.append(x)
---
+++ codeforces/2211a.py
@@ -0,0 +1,11 @@
+# t = int(input())
+# for _ in range():
+#     n = int(input())
+#     p = list(map(int, input().split()))
+
+#     if n < 3:
+#         print(n)
+
+
+
+print(2%3)
