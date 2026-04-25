9f4ebf21c3131a39c7408fb6e20775b38e6d15a6
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2026-04-25 20:30:29 +0700

    leetcode: Removes Duplicate form sorted array

---
+++ leetcode/array_26.py
@@ -0,0 +1,19 @@
+# Removes Duplicate form sorted array
+
+def remove_duplicate(nums):
+
+    if not nums:
+        return 0
+
+    i = 0
+
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+
+    return i + 1
+
+
+print(remove_duplicate([1,1,2]))
+print(remove_duplicate([0,0,1,1,1,2,2,3,3,4]))
