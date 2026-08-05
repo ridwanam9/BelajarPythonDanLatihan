9815d96f4539e06bc06fa2ce34e5e50f86b9f4da
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2026-08-05 08:47:40 +0700

    cd_2248a

---
+++ codeforces/2248a.py
@@ -0,0 +1,11 @@
+t = int(input())
+for _ in range(t):
+    s = list(input())
+    s.remove("0")
+    s.remove("1")
+    print("".join(s))
+
+    
+
+
+
---
+++ codeforces/2250a.py
@@ -0,0 +1,27 @@
+# t = int(input())
+# for _ in range(t):
+#     n = int(input())
+#     w = list(map(int, input().split()))
+
+#     bools = [True]*n
+#     k = 1
+#     if n == 1:
+#         print("no")
+#     elif n == 2:
+#         if abs(w[0]-w[1]) <= 1:
+#             print("no")
+#         else:
+#             if w[0] < w[1]:
+#                 print("no")
+#             else:
+#                 print("yes")
+#     else:
+#         for i in range(n):
+#             if w[0] < k:
+#                 bools[i] == False
+
+
+
+
+
+
