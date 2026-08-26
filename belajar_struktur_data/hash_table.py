58685d639455af8add9a29f18d1e5721d2b7f31e
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2026-08-26 10:16:56 +0700

    hash table: remove & check name

--- belajar_struktur_data/hash_table.py
+++ belajar_struktur_data/hash_table.py
@@ -11,10 +11,8 @@
 #   [],
 #   []
 # ]
-# names = [[]]*10
 names = [[] for _ in range(9)]
-# print(names)
-# print(names1)
+
 
 def index_position(name):
     count = 0
@@ -31,18 +29,22 @@ def contains(name):
     # return names[position] == name #akan menghasilkan false karena membandingkan names[position] = list dengan name = string 
     return name in names[position]
 
+def remove(name):
+    position = index_position(name)
+    names[position].remove(name)
 
+def check_name(name):
+    return f"{name} is in the Hash Table: {contains(name)}"
 
-add('Pete')
-add('Jones')
-add('Lisa')
-add('Siri')
 add('Stuart')
 add('Helda')
 add('Johan')
 add("Yusuf")
+
 print(names)
-print("'Pete' is in the Hash Table:", contains("Pete"))
-print("'Jones' is in the Hash Table:", contains("Jones"))
-# add("Noah")
-# print(names)
+print(check_name("Helda"))
+
+remove('Helda')
+
+print(names)
+print(check_name("Helda"))
